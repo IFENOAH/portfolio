aea6d06d22a999fcc0ebcdf9fa142c193e8007f9
Ifeoluwadayo Noah <ogunderuife@yahoo.com>
2022-03-21 16:41:31 +0100

    adding skill and project swap

--- src/components/ProjectsPage/Projects.js
+++ src/components/ProjectsPage/Projects.js
@@ -1,43 +1,59 @@
-import React from 'react'
-import styles from './Projects.module.css'
+import React from "react";
+import styles from "./Projects.module.css";
 
 function Projects() {
-    return (
-        <div className = {styles.project_container}> 
+  return (
+    <div className={styles.project_container}>
+      <h2 className={styles.project_header}>MY WORK</h2>
 
-            <h2 className = {styles.project_header}>MY WORK</h2>
+      <div className={styles.project_holder}>
+        <a
+          href="https://sifuse.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={styles.projects1}
+        >
+          <h4>SIFUSE</h4>
+          <h6 className={styles.tool}> NEXT.JS/REDUX/TAILWIND/SCSS</h6>
+        </a>
 
-            <div className = { styles.project_holder }>
+        <a
+          href="https://fartra.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={styles.projects2}
+        >
+          <h4>FARTRA</h4>
+          <h6 className={styles.tool}>HTML/SCSS/JAVASCRIPT</h6>
+        </a>
 
-                <a href =  "https://sifuse.com/" target="_blank" rel="noopener noreferrer"  className = {styles.projects1} >
-                    <h4>SIFUSE</h4>
-                    <h6 className = {styles.tool}> NEXT.JS/REDUX/TAILWIND/SCSS</h6>
-                </a>
+        <a
+          href="https://net-cloned.netlify.app"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={styles.projects3}
+        >
+          <h4>NETFLIX CLONE</h4>
+          <h6 className={styles.tool}>REACT</h6>
+        </a>
 
-                <a href = "https://fartra.netlify.app/" target="_blank" rel="noopener noreferrer" className = {styles.projects2} >
-                    <h4>FARTRA</h4>
-                    <h6 className = {styles.tool}>HTML/SCSS/JAVASCRIPT</h6>
-                </a>
+        <a
+          href="https://checktemp.netlify.app"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={styles.projects4}
+        >
+          <h4>CHECKTEMP</h4>
+          <h6 className={styles.tool}>REACT</h6>
+        </a>
+      </div>
 
-                <a  href =  "https://net-cloned.netlify.app" target="_blank" rel="noopener noreferrer" className = {styles.projects3} >
-                    <h4>NETFLIX CLONE</h4>
-                    <h6 className = {styles.tool}>REACT</h6>
-                </a>
-
-                <a href = "https://ifenoah.github.io/DevNoah/" target="_blank" rel="noopener noreferrer" className = {styles.projects4} >
-                    <h4>OLD FOLIO</h4>
-                    <h6 className = {styles.tool}>REACT</h6>
-                </a>
-
-            </div>
-
-            {/* <button className = {styles.projectButton} data-aos='fade-up'>
+      {/* <button className = {styles.projectButton} data-aos='fade-up'>
                 nice work? <br></br>
                 Exlpore More
             </button> */}
-
-        </div>
-    )
+    </div>
+  );
 }
 
-export default Projects
+export default Projects;
--- src/components/StackPage/StackPage.js
+++ src/components/StackPage/StackPage.js
@@ -1,41 +1,75 @@
-import React from 'react'
-import styles from './StackPage.module.css'
-import image2 from '../../Images/html_icon.svg'
-import image3 from '../../Images/css.png'
-import image4 from '../../Images/sass_icon.svg'
-import image5 from '../../Images/tailwind.svg'
-import image6 from '../../Images/js_logo.svg'
-import image7 from '../../Images/react_icon.svg'
-import image8 from '../../Images/redux.svg'
-import image9 from '../../Images/next_icon.svg'
-import image10 from '../../Images/git.svg'
-import image11 from '../../Images/photoshop.png'
-
+import React from "react";
+import styles from "./StackPage.module.css";
+import image2 from "../../Images/html_icon.svg";
+import image3 from "../../Images/css.png";
+import image4 from "../../Images/sass_icon.svg";
+import image5 from "../../Images/tailwind.svg";
+import image6 from "../../Images/js_logo.svg";
+import image7 from "../../Images/react_icon.svg";
+import image8 from "../../Images/redux.svg";
+import image9 from "../../Images/next_icon.svg";
+import image10 from "../../Images/git.svg";
+import image11 from "../../Images/photoshop.png";
+import image12 from "../../Images/typescript.svg";
 
 function StackPage() {
-    return (
-        <div className = {styles.stack_container}>
+  return (
+    <div className={styles.stack_container}>
+      <h2 className={styles.stack_header}>STACK</h2>
 
-        <h2 className = {styles.stack_header} >STACK</h2>
+      <span className={styles.text}>
+        {" "}
+        I make use of technologies and languages such as:{" "}
+      </span>
 
-        <span className = {styles.text}> I make use of technologies and languages such as: </span>
-
-        <div className = { styles.stack_holder }>
-            <span className = {styles.tool} > <img src = {image2} alt = "dot" className = { styles.img }></img> HTML </span>
-            <span className = {styles.tool} > <img src = {image3} alt = "dot" className = { styles.img }></img>CSS</span>
-            <span className = {styles.tool} > <img src = {image4} alt = "dot" className = { styles.img }></img>SCSS</span>
-            <span className = {styles.tool} > <img src = {image5} alt = "dot" className = { styles.img2 }></img>TAILWIND CSS</span>
-            <span className = {styles.tool} > <img src = {image6} alt = "dot" className = { styles.img2 }></img>JAVASCRIPT</span>
-            <span className = {styles.tool} > <img src = {image7} alt = "dot" className = { styles.img3 }></img>REACT</span>
-            <span className = {styles.tool} > <img src = {image8} alt = "dot" className = { styles.img3 }></img>REDUX</span>
-            <span className = {styles.tool} > <img src = {image9} alt = "dot" className = { styles.img3 }></img>NEXT.JS</span>
-            <span className = {styles.tool} > <img src = {image10} alt = "dot" className = { styles.img }></img>GIT</span>
-            <span className = {styles.tool} > <img src = {image11} alt = "dot" className = { styles.img2 }></img>PHOTOSHOP</span>
-        </div>
-
-
+      <div className={styles.stack_holder}>
+        <span className={styles.tool}>
+          {" "}
+          <img src={image2} alt="dot" className={styles.img}></img> HTML{" "}
+        </span>
+        <span className={styles.tool}>
+          {" "}
+          <img src={image3} alt="dot" className={styles.img}></img>CSS
+        </span>
+        <span className={styles.tool}>
+          {" "}
+          <img src={image4} alt="dot" className={styles.img}></img>SCSS
+        </span>
+        <span className={styles.tool}>
+          {" "}
+          <img src={image5} alt="dot" className={styles.img2}></img>TAILWIND CSS
+        </span>
+        <span className={styles.tool}>
+          {" "}
+          <img src={image6} alt="dot" className={styles.img2}></img>JAVASCRIPT
+        </span>
+        <span className={styles.tool}>
+          {" "}
+          <img src={image12} alt="dot" className={styles.img2}></img>TYPESCRIPT
+        </span>
+        <span className={styles.tool}>
+          {" "}
+          <img src={image7} alt="dot" className={styles.img3}></img>REACT
+        </span>
+        <span className={styles.tool}>
+          {" "}
+          <img src={image8} alt="dot" className={styles.img3}></img>REDUX
+        </span>
+        <span className={styles.tool}>
+          {" "}
+          <img src={image9} alt="dot" className={styles.img3}></img>NEXT.JS
+        </span>
+        <span className={styles.tool}>
+          {" "}
+          <img src={image10} alt="dot" className={styles.img}></img>GIT
+        </span>
+        <span className={styles.tool}>
+          {" "}
+          <img src={image11} alt="dot" className={styles.img2}></img>PHOTOSHOP
+        </span>
+      </div>
     </div>
-    )
+  );
 }
 
-export default StackPage
+export default StackPage;
