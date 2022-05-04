f5b8253552bd01dfe486be8b0f58f89cfdcf6fe4
Ogunderu Noah Ife <ifeoluwadayonoah@gmail.com>
2022-05-04 15:47:58 +0100

    fix resume for netlify

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./AboutPage.module.css";
-import Pdf from "../../Documents/Newresume.pdf";
+import Pdf from "../../Documents/newResume.pdf";
 
 function AboutPage() {
   return (
