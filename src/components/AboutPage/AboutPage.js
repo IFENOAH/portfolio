07717e18cb7f3b57c65cda505fd92c5097498d99
Ogunderu Noah Ife <ifeoluwadayonoah@gmail.com>
2023-10-11 09:55:22 +0100

    cv update

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./AboutPage.module.css";
-import Pdf from "../../Documents/noah's_resume.pdf";
+import Pdf from "../../Documents/NOAH-FRONTEND-CV.pdf";
 
 function AboutPage() {
   return (
