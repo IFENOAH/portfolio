b8a23cdbef777f9ec605c9d640b2415eb6e14cb7
Ogunderu Ifeoluwadayo Noah <ifeoluwadayonoah@gmail.com>
2025-11-14 17:42:04 +0100

    Hero fix

--- src/components/HeroPage/HeroPage.js
+++ src/components/HeroPage/HeroPage.js
@@ -1,64 +1,56 @@
-import React from 'react'
-import styles from './HeroPage.module.css'
-import image1 from '../../Images/ellipse.svg'
-import image2 from '../../Images/Arrow-Down.svg'
+import React from "react";
+import styles from "./HeroPage.module.css";
+import image1 from "../../Images/ellipse.svg";
+import image2 from "../../Images/Arrow-Down.svg";
 
 function HeroPage() {
-    return (
+  return (
+    <div className={styles.hero_page}>
+      <div className={styles.hero_header}>
+        NOAH
+        <img src={image1} alt="dot" className={styles.dot}></img> IFE
+      </div>
 
-        <div className ={ styles.hero_page } >
+      <div className={styles.hero_content}>
+        <span>
+          <span className={styles.hero_letter}>S</span>
+          <span className={styles.hero_letter}>O</span>
+          <span className={styles.hero_letter}>F</span>
+          <span className={styles.hero_letter}>T</span>
+        </span>
 
-            <div className =  { styles.hero_header }>
-                NOAH 
-                <img src = {image1} alt = "dot" className = { styles.dot }>
-                </img> IFE 
-            </div>
+        <span>
+          <span className={styles.hero_letter}>W</span>
+          <span className={styles.hero_letter}>A</span>
+          <span className={styles.hero_letter}>R</span>
+          <span className={styles.hero_letter}>E</span>
+        </span>
 
-            <div className = {styles.hero_content } > 
+        <br></br>
 
-                <span>
-                    <span className = {styles.hero_letter} >S</span>
-                    <span className = {styles.hero_letter} >O</span>
-                    <span className = {styles.hero_letter} >F</span>
-                    <span className = {styles.hero_letter} >T</span>
-                    <span className = {styles.hero_letter} >T</span>
-                </span>
+        <span>
+          <span className={styles.hero_letter}>-</span>
+          <span className={styles.hero_letter}>E</span>
+          <span className={styles.hero_letter}>N</span>
+          <span className={styles.hero_letter}>G</span>
+          <span className={styles.hero_letter}>I</span>
+        </span>
 
-                <span>
-                    <span className = {styles.hero_letter} >W</span>
-                    <span className = {styles.hero_letter} >A</span>
-                    <span className = {styles.hero_letter} >R</span>
-                    <span className = {styles.hero_letter} >E</span>
-                </span>
+        <span>
+          <span className={styles.hero_letter}>N</span>
+          <span className={styles.hero_letter}>E</span>
+          <span className={styles.hero_letter}>E</span>
+          {/* <span className={styles.hero_letter}>E</span> */}
+          <span className={styles.hero_letter}>R</span>
+        </span>
 
-                <br></br>
-
-                <span >
-                    <span className = {styles.hero_letter} >D</span>
-                    <span className = {styles.hero_letter} >E</span>
-                    <span className = {styles.hero_letter} >V</span>
-                    <span className = {styles.hero_letter} >E</span>
-                </span>
-
-                <span>
-                    <span className = {styles.hero_letter} >L</span>
-                    <span className = {styles.hero_letter} >O</span>
-                    <span className = {styles.hero_letter} >P</span>
-                    <span className = {styles.hero_letter} >E</span>
-                    <span className = {styles.hero_letter} >R</span>
-                </span>
-
-
-                <div className = {styles.scroll_button}>
-                    <span className = { styles.scroll } >Scroll</span>
-                    <img src = { image2 } alt = "arrow" className ={ styles.scroll_icon }></img>
-                </div>
-
-            </div>
-
-        
+        <div className={styles.scroll_button}>
+          <span className={styles.scroll}>Scroll</span>
+          <img src={image2} alt="arrow" className={styles.scroll_icon}></img>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default HeroPage
+export default HeroPage;
